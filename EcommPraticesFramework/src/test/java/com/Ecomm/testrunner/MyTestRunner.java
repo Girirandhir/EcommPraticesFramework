09ea78c10a02ce2a353724b7eebe41c1bdc6fb05
Raj <Raj@DESKTOP-OLHT9HB>
2021-01-03 15:47:48 +0530

    Updating runner

--- EcommPraticesFramework/src/test/java/com/Ecomm/testrunner/MyTestRunner.java
+++ EcommPraticesFramework/src/test/java/com/Ecomm/testrunner/MyTestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = {"src/test/resources/Appfeatures"},
+		features = {"src/test/resources/Appfeatures/login.feature"},
 		glue= {"com/Ecomm/stepdefination","com/Ecomm/AppHooks"},
 		plugin= {"pretty", 
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
